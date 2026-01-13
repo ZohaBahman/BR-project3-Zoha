b31566e73433626bbd8a153ba019640c44d82009
“ZohaBahman” <zoha.bahman@gmail.com>
2026-01-13 18:28:01 +0100

    navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
 import { Link } from 'react-router-dom'
+import logo from '../assets/spoon-logo.png'
 
 function Navbar() {
     return (
         <nav className="navbar">
-            <Link to="/">Home</Link>
-            <Link to="/favorites">Favorites</Link>
+            <div className="navbar-inner">
+                <div className="logo">
+                    <Link to="/">
+                        <img src={logo} alt="logo" />
+                    </Link>
+                </div>
+
+                <div className="nav-links">
+                    <Link to="/">Home</Link>
+                    <Link to="/favorites">Favorites</Link>
+                </div>
+            </div>
         </nav>
     )
 }
