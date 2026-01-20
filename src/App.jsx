2366788dc6bc1945752a6d4326525a126fb6c0ef
“ZohaBahman” <zoha.bahman@gmail.com>
2026-01-20 18:10:22 +0100

    file name changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Favorites from './pages/Favorites'
-import RecipeDetails from './pages/recipeDetails'
+import RecipeDetails from './pages/RecipeDetails'
 import Navbar from './components/Navbar'
 
 
